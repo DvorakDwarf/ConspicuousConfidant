ef8e95336bf00a94d0faf35321036cf7d4ae6535
HistidineDwarf <dvorakdwarf@gmail.com>
2025-04-19 13:51:35 -0700

    Messaging with background and troll function

--- background.js
+++ background.js
@@ -4,4 +4,27 @@ function getTabs() {
 
 document.addEventListener("DOMContentLoaded", getTabs);
 
+browser.runtime.onMessage.addListener((data, sender) => {
+    if (data.type === "switch_tab") {
+        console.log("Switching tabs");
+
+        browser.tabs.query();
+        return Promise.resolve("done");
+    }
+    return false;
+});
+
+function logTabs(tabs) {
+    for (const tab of tabs) {
+        // tab.url requires the `tabs` permission or a matching host permission.
+        console.log(tab.url);
+    }
+}
+  
+function onError(error) {
+    console.error(`Error: ${error}`);
+}
+  
+browser.tabs.query({}).then(logTabs, onError);
+
 console.log("BACKGROUND LOADED")
--- confidant.js
+++ confidant.js
@@ -1,1 +1,18 @@
 console.log("WORKING");
+
+//Send message to background
+function backToProductivity() {
+    console.log("Sending to productivity tab");
+    browser.runtime.sendMessage({
+        type: "switch_tab"
+    }); 
+}
+
+function troll() {
+    //Mess with html here
+    // document.body.style.border = "5px solid red";
+}
+
+setTimeout(backToProductivity, 1000 * 1);
+
+setInterval(troll, 1000 * 5);
