49a2e6e37248e50672fc9d64337bceded7650688
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2025-04-19 14:04:31 -0700

    e

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,17 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'manifest.json',
-          dest: '.',
-        }
-      ],
-    }),
-  ],
+  plugins: [react()],
   build: {
-    outDir: 'build',
     rollupOptions: {
       input: {
-        main: './index.html',
-      },
+        popup: resolve(__dirname, 'popup/popup.html')
+      }
     },
-  },
-});
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
