f96b4d61e4229dd2f4bee2719b5da74deeb0cc78
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2025-04-19 21:08:35 -0700

    non-functional front-end changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "webextension-polyfill";
+// import Menu from "./icons/Menu.svg"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
